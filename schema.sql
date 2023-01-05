df506dfd019a0f3c0fc103c2af5b99c1a7f2e549
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-05 12:30:21 -0700

    update var count limit

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 create table music (
     id serial primary key,
-    coverArt varchar(50) not null,
+    coverArt varchar(200) not null,
     artist varchar(50) not null,
     genre varchar(50) not null,
     title varchar(50) not null,
