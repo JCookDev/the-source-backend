94284dff71230e6187e69cb83ff465f315e49ff2
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-05 11:36:56 -0700

    Create the schema.sql file

---
+++ schema.sql
@@ -0,0 +1,8 @@
+create table music (
+    id serial primary key,
+    coverArt varchar(50) not null
+    artist varchar(50) not null
+    genre varchar(50) not null
+    title varchar(50) not null
+    audioFile varchar(50) not null
+);
