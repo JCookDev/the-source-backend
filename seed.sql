7b875a24430f8b5ecceb7ab75ef82d08d375c879
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-05 12:03:11 -0700

    Seed the sample data to SQL

---
+++ seed.sql
@@ -0,0 +1,9 @@
+insert into music
+    (coverArt, artist, genre, title, audioFile)
+values
+    ('image', 'future', 'Hip-Hop', 'Puffin On Zooties', 'file'),
+    ('image', 'Bob Marley', 'Hip-Hop', 'Sprite', 'file'),
+    ('image', 'Miss Aphrodite', 'R&B', 'Your Hands', 'file'),
+    ('image', 'Bobby Blue Bland', 'Blues', 'Aint No Sunshine When Shes Gone', 'file'),
+    ('image', 'Metallica', 'The Four Horsemen', 'Rock', 'file')
+;
