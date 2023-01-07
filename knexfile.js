f02de7fe68c1c9d954605e65d216089f3cfa9532
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-06 19:35:41 -0700

    Initialize knex files

---
+++ knexfile.js
@@ -0,0 +1,55 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      port: 5432,
+      user: "postgres",
+      password: "postgres",
+      database: "the-source-backend",
+      // filename: './dev.sqlite3'
+    }
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'the-source-backend',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      // database: 'the-source-backend',
+      // user:     'username',
+      // password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
--- server.js
+++ server.js
@@ -1,76 +1,79 @@
-const { response } = require('express');
-const express = require('express');
+const { response } = require("express");
+const express = require("express");
+const knex = require("./knex");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
 //-- Security
-app.use(cors())
+app.use(cors());
 
 //-- Needed for a post request
-app.use(express.json())
+app.use(express.json());
 
-app.locals.music = [
-    {
-        id: 1,
-        coverArt: 'image',
-        artist: 'Future',
-        genre: 'Hip-Hop',
-        title: 'Puffin On Zooties',
-        audioFile: 'file',
-      },
-      {
-        id: 2,
-        coverArt: 'https://upload.wikimedia.org/wikipedia/en/f/f8/Is_This_Love_%28Bob_Marley_%26_The_Wailers_single_-_cover_art%29.jpg',
-        artist: 'Bob Marley',
-        genre: 'Hip-Hop',
-        title: 'Sprite',
-        audioFile: 'file',
-      },
-      {
-        id: 3,
-        coverArt: 'image',
-        artist: 'Miss Aphrodite',
-        genre: 'R&B',
-        title: 'Your Hands',
-        audioFile: 'file',
-      },
-      {
-        id: 4,
-        coverArt: 'image',
-        artist: 'Bobby Blue Bland',
-        genre: 'Blues',
-        title: 'Ain\’t No Sunshine When Shes Gone',
-        audioFile: 'file',
-      },
-]
+// app.locals.music = [
+//   {
+//     id: 1,
+//     coverArt: "image",
+//     artist: "Future",
+//     genre: "Hip-Hop",
+//     title: "Puffin On Zooties",
+//     audioFile: "file",
+//   },
+//   {
+//     id: 2,
+//     coverArt:
+//       "https://upload.wikimedia.org/wikipedia/en/f/f8/Is_This_Love_%28Bob_Marley_%26_The_Wailers_single_-_cover_art%29.jpg",
+//     artist: "Bob Marley",
+//     genre: "Hip-Hop",
+//     title: "Sprite",
+//     audioFile: "file",
+//   },
+//   {
+//     id: 3,
+//     coverArt: "image",
+//     artist: "Miss Aphrodite",
+//     genre: "R&B",
+//     title: "Your Hands",
+//     audioFile: "file",
+//   },
+//   {
+//     id: 4,
+//     coverArt: "image",
+//     artist: "Bobby Blue Bland",
+//     genre: "Blues",
+//     title: "Ain’t No Sunshine When Shes Gone",
+//     audioFile: "file",
+//   },
+// ];
 
-app.set('port', 3001);
-app.locals.title = 'The Source';
+app.set("port", 3001);
+app.locals.title = "The Source";
 
-app.get('/', (request, response) => {
-  response.send('Oh hey The Source');
+app.get("/", (request, response) => {
+  response.send("Oh hey The Source");
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+app.listen(app.get("port"), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get("port")}.`);
 });
 
-app.get('/music', (request, response) => {
-    const music = app.locals.music
-	response.status(200).json({music})
-})
+app.get("/music", async(request, response) => {
+  const music = await knex.select().from("music")
+  // const music = app.locals.music;
+  response.status(200).json({ music });
+});
 
 // app.get('/music', (request, response) => {
 //     response.status(200).json(app.locals.music);
 //   });
 
-app.post('/music', (request, response) => {
-    const id = Date.now();
-    const { coverArt, artist, genre, title, audioFile } = request.body;
+app.post("/music", (request, response) => {
+  const id = Date.now();
+  const { coverArt, artist, genre, title, audioFile } = request.body;
 
-    app.locals.music.push({ id, coverArt, artist, genre, title, audioFile });
+  app.locals.music.push({ id, coverArt, artist, genre, title, audioFile });
 
-    response.status(201).json({ id, coverArt, artist, genre, title, audioFile });
+  response.status(201).json({ id, coverArt, artist, genre, title, audioFile });
 });
 // app.post('/music', (request, response) => {
 //     const id = Date.now()
@@ -80,16 +83,16 @@ app.post('/music', (request, response) => {
 //         if (!music[requiredParameter]) {
 //          /*return */ response
 //             .status(422)
-//             .send({ error: 
-//                 `Expected format: 
-//                 { 
-//                     id: <Number>, 
-//                     coverArt: <String>, 
-//                     artist: <String>, 
-//                     genre: <String>, 
-//                     title: <String>, 
-//                     audioFile: <String> 
-//                 }. You're missing a "${requiredParameter}" property.` 
+//             .send({ error:
+//                 `Expected format:
+//                 {
+//                     id: <Number>,
+//                     coverArt: <String>,
+//                     artist: <String>,
+//                     genre: <String>,
+//                     title: <String>,
+//                     audioFile: <String>
+//                 }. You're missing a "${requiredParameter}" property.`
 //             });
 //         }
 //       }
