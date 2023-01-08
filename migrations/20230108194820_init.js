5b3e3363b22503bb350710b9d3d6c6281ea59955
Jeff Cook <cookturingedu@gmail.com>
2023-01-08 14:51:45 -0500

    Add template to create database table in migrations file

--- migrations/20230108194820_init.js
+++ migrations/20230108194820_init.js
@@ -3,7 +3,15 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('music', table => {
+        table.increments('id')
+        table.string('cover_art', 500)
+        table.string('artist', 250)
+        table.string('genre', 250)
+        table.string('title', 250)
+        table.string('audio_file', 500)
+        table.timestamps(true, true)
+    })
 };
 
 /**
@@ -11,5 +19,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+   
 };
