b6073b5e5e3c59cacae7dd3abce1fb0168f22ebb
Jenn Yacoubian <jyacoubian@gmail.com>
2023-01-08 16:12:19 -0700

    Change back to node

--- db/seed.sql
+++ db/seed.sql
@@ -1,9 +1,9 @@
-insert into music
-    (coverArt, artist, genre, title, audioFile)
-values
-    ('https://images.genius.com/e14ea3e9d432ebf403c2ee8cf8a6b662.1000x1000x1.png', 'future', 'Hip-Hop', 'Puffin On Zooties', 'file'),
-    ('https://upload.wikimedia.org/wikipedia/en/f/f8/Is_This_Love_%28Bob_Marley_%26_The_Wailers_single_-_cover_art%29.jpg', 'Bob Marley', 'Reggae', 'IT This Love', 'file'),
-    ('https://i.ytimg.com/vi/S1QasDs5Jcc/maxresdefault.jpg', 'Miss Aphrodite', 'R&B', 'Your Hands', 'file'),
-    ('https://i.scdn.co/image/ab67616d0000b2736c30c36a8657487454f46642', 'Bobby Blue Bland', 'Blues', 'Aint No Sunshine When Shes Gone', 'file'),
-    ('https://www.metallica.com/on/demandware.static/-/Sites-Metallica-Library/default/dw6503eaa3/images/releases/20150807_213844_7549_752889.jpeg', 'Metallica', 'The Four Horsemen', 'Rock', 'file')
-;
+-- insert into music
+--     (coverArt, artist, genre, title, audioFile)
+-- values
+--     ('https://images.genius.com/e14ea3e9d432ebf403c2ee8cf8a6b662.1000x1000x1.png', 'future', 'Hip-Hop', 'Puffin On Zooties', 'file'),
+--     ('https://upload.wikimedia.org/wikipedia/en/f/f8/Is_This_Love_%28Bob_Marley_%26_The_Wailers_single_-_cover_art%29.jpg', 'Bob Marley', 'Reggae', 'IT This Love', 'file'),
+--     ('https://i.ytimg.com/vi/S1QasDs5Jcc/maxresdefault.jpg', 'Miss Aphrodite', 'R&B', 'Your Hands', 'file'),
+--     ('https://i.scdn.co/image/ab67616d0000b2736c30c36a8657487454f46642', 'Bobby Blue Bland', 'Blues', 'Aint No Sunshine When Shes Gone', 'file'),
+--     ('https://www.metallica.com/on/demandware.static/-/Sites-Metallica-Library/default/dw6503eaa3/images/releases/20150807_213844_7549_752889.jpeg', 'Metallica', 'The Four Horsemen', 'Rock', 'file')
+-- ;
