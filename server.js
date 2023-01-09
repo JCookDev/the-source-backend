6cfeda88940c2d339368c6768f7599bd1eee88fd
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-09 15:38:02 -0700

    refactor server communication with database

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ app.use(express.json());
 app.set("port", process.env.PORT || 3001);
 app.locals.title = "The Source";
 
-app.listen(app.get("port"), () => {
-  console.log(`${app.locals.title} is running on http://localhost:${app.get("port")}.`);
-});
-
 app.get('/', (request, response) => {
   response.status(200).json({
     smoke: "test"
@@ -25,18 +21,19 @@ app.get('/', (request, response) => {
 
 app.get("/api/v1/music", async (request, response) => {
   const tracks = await knex.select().from("tracks");
+  // console.log("EXPRESS TRACKS FROM GET=====", tracks)
   response.status(200).json( {tracks} );
 });
 
 app.post("/api/v1/music", async (request, response) => {
   const tracks = request.body;
-  for (let requiredParameter of ["id", "coverart", "artist", "genre", "title", "audiofile"]) {
-    if (!tracks[requiredParameter]) {
+  const requiredParams = ["id", "coverart", "artist", "genre", "title", "audiofile"]
+  for (let requiredParameter of requiredParams) {
+    if (tracks[requiredParameter] === undefined) {
       return response
         .status(422)
         .send({  error: `Expected format:
             {
-                id: <Number>,
                 coverart: <String>,
                 artist: <String>,
                 genre: <String>,
@@ -46,11 +43,20 @@ app.post("/api/v1/music", async (request, response) => {
       });
     }
   }
-  response.status(201).json( request.body );
-  try {
-    const tracks = await knex('tracks').insert(request.body, ['id', 'coverart', 'artist', 'genre', 'title', 'audiofile'])
-    response => response.status(201).json({ tracks })
-  } catch (error) {
-    response.status(500).json(error)
-  }
+  // response.status(201).json( request.body );
+
+      const tracks2 = await knex('tracks').insert(request.body, ['id', 'coverart', 'artist', 'genre', 'title', 'audiofile'])
+      console.log("EXPRESS TRACKS =====", tracks2)
+      response.status(201).json( tracks2[0] )
+
+  // try {
+  //   const tracks = await knex('tracks').insert(request.body, ['id', 'coverart', 'artist', 'genre', 'title', 'audiofile'])
+  //     response.status(201).json({ tracks })
+  // } catch (error) {
+  //   response.status(500).json(error)
+  // }
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get("port")}.`);
 });
