1fa0460d58db14f0074bec9a46250cc2856d7a10
Jenn Yacoubian <jyacoubian@gmail.com>
2023-01-08 19:20:02 -0700

    Change dev environment

--- knex.js
+++ knex.js
@@ -1,6 +1,6 @@
 const knex = require("knex");
 const config = require("./knexfile");
-const environment = process.env.DB_ENV || "development";
+const environment = process.env.DATABASE_URL || "development";
 const knexConfig = config[environment];
 module.exports = knex(knexConfig);
 
