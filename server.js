10190aca6e821bcd05f724458ebfefa05da2a0eb
Lauren Black <laurenblack5280@gmail.com>
2023-01-08 20:13:06 -0700

    reinstall nodemon package, remove node 16.15.1 from engines, using most current node version, 18

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+//--- Simplified server for deployment on fly.io
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get(["/", "/:name"], (req, res) => {
+  greeting = "<h1>Hello From Node on Fly!</h1>";
+  name = req.params["name"];
+  if (name) {
+    res.send(greeting + "</br>and hello to " + name);
+  } else {
+    res.send(greeting);
+  }
+});
+
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
+
+//--- This server works on POSTMAN, but could not deploy on heroku
+
 // const express = require("express");
 // const cors = require("cors");
 // const knex = require("./knex");
