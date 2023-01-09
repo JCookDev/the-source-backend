c1b73f286f952d9f8356c099aa40032b0572079d
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-09 15:31:57 -0700

    re-enable knex del

--- seeds/tracks-seed.js
+++ seeds/tracks-seed.js
@@ -4,7 +4,7 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  //await knex('table_name').del()
+  await knex('tracks').del()
   await knex('tracks').insert([
     {id: 1, coverart: 'https://images.genius.com/e14ea3e9d432ebf403c2ee8cf8a6b662.1000x1000x1.png', artist: 'future', genre: 'Hip-Hop' , title: 'Puffin On Zooties', audiofile: 'file'},
     {id: 2, coverart: 'https://upload.wikimedia.org/wikipedia/en/f/f8/Is_This_Love_%28Bob_Marley_%26_The_Wailers_single_-_cover_art%29.jpg', artist: 'Bob Marley', genre: 'Reggae' , title: 'Is This Love' , audiofile: 'file'},
